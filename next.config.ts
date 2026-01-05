dd0c1ffba22c8a807b879a67dbbb229b12a30b9f
Lawrence Reynolds <lawrence.m.reynolds@googlemail.com>
2026-01-05 16:01:44 +0000

    Fixed issue with mathematiacal equations in md files.

--- next.config.ts
+++ next.config.ts
@@ -3,11 +3,12 @@ import nextra from 'nextra'
 // Set up Nextra with its configuration
 const withNextra = nextra({
   // ... Add Nextra-specific options here
+  latex: true
 })
  
 // Export the final Next.js config with Nextra included
 export default withNextra({
   // ... Add regular Next.js options here
   // contentDirBasePath: '/docs'
-  latex: true
+  
 })
